a73968178b1b748925ebe2e253b11ee3b2c8e0b3
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-03-31 23:39:03 +0300

    refactor(machine/enigma/base): add constants

--- pkg/machine/enigma/base/consts.go
+++ pkg/machine/enigma/base/consts.go
@@ -3,8 +3,10 @@ package base
 const (
 	CharsCount26 byte = 26
 
-	RotorsCount3 = 3
-	RotorsCount4 = 4
+	ReflectorsCount1 = 1
+	RotorsCount3     = 3
+	RotorsCount4     = 4
+	PlugboardsCount1 = 1
 
 	UpperA byte = 65
 	UpperZ byte = 90
