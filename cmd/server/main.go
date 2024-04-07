d0e8507c4e58ebdc2f523e13719bbf5458a0fe55
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-04-07 20:04:29 +0300

    feat(cmd/server): complete text encryption handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/mrumyantsev/cipher-machines-app/pkg/machine"
 	"github.com/mrumyantsev/cipher-machines-app/pkg/machine/enigma"
+	"github.com/mrumyantsev/cipher-machines-app/pkg/machine/enigma/base"
 	"github.com/mrumyantsev/logx/log"
 )
 
@@ -84,9 +86,59 @@ func encryptText(c echo.Context) error {
 		return fmt.Errorf("could not unmarshal json to PlaintextMsg struct: %v", err)
 	}
 
-	fmt.Println(p)
+	ct, err := ciphertext(*p)
+	if err != nil {
+		return fmt.Errorf("could not encrypt plaintext: %v", err)
+	}
 
-	return c.JSON(200, CiphertextMsg{Ciphertext: "XGWBSW"})
+	return c.JSON(200, ct)
+}
+
+func ciphertext(plaintext PlaintextMsg) (CiphertextMsg, error) {
+	var m machine.Machiner
+
+	if strings.EqualFold(plaintext.Machine, `Enigma I "Norenigma"`) {
+		m = enigma.NewEnigmaINE(
+			plaintext.Settings.Reflector,
+			[base.RotorsCount3]machine.RotorSettings(plaintext.Settings.RotorSettings),
+			plaintext.Settings.Plugboard,
+		)
+	} else if strings.EqualFold(plaintext.Machine, `Enigma I "Sondermaschine"`) {
+		m = enigma.NewEnigmaISM(
+			plaintext.Settings.Reflector,
+			[base.RotorsCount3]machine.RotorSettings(plaintext.Settings.RotorSettings),
+			plaintext.Settings.Plugboard,
+		)
+	} else if strings.EqualFold(plaintext.Machine, "Enigma M3") {
+		m = enigma.NewEnigmaM3(
+			plaintext.Settings.Reflector,
+			[base.RotorsCount3]machine.RotorSettings(plaintext.Settings.RotorSettings),
+			plaintext.Settings.Plugboard,
+		)
+	} else if strings.EqualFold(plaintext.Machine, `Enigma M4 "Shark"`) {
+		m = enigma.NewEnigmaM4S(
+			plaintext.Settings.Reflector,
+			[base.RotorsCount4]machine.RotorSettings(plaintext.Settings.RotorSettings),
+			plaintext.Settings.Plugboard,
+		)
+	} else {
+		m = enigma.NewEnigmaI(
+			plaintext.Settings.Reflector,
+			[base.RotorsCount3]machine.RotorSettings(plaintext.Settings.RotorSettings),
+			plaintext.Settings.Plugboard,
+		)
+	}
+
+	ciphertext := CiphertextMsg{}
+
+	text, err := m.EncryptString(plaintext.Plaintext)
+	if err != nil {
+		return ciphertext, fmt.Errorf("could not encrypt via machine: %v", err)
+	}
+
+	ciphertext.Ciphertext = text
+
+	return ciphertext, nil
 }
 
 type PlaintextMsg struct {
