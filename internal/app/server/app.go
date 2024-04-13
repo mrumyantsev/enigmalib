1a9ae87d3218e83a6267c814660a087a60e99631
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-04-13 18:42:41 +0300

    refactor(app/server): apply new code structure

---
+++ internal/app/server/app.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/mrumyantsev/cipher-machines-app/internal/pkg/config"
+	"github.com/mrumyantsev/cipher-machines-app/internal/pkg/endpoint"
+	"github.com/mrumyantsev/cipher-machines-app/internal/pkg/server"
+	"github.com/mrumyantsev/cipher-machines-app/internal/pkg/service"
+	"github.com/mrumyantsev/cipher-machines-app/pkg/lib/errlib"
+	"github.com/mrumyantsev/logx/log"
+)
+
+type App struct {
+	config   *config.Config
+	service  *service.Service
+	endpoint *endpoint.Endpoint
+	server   *server.Server
+}
+
+func New() (*App, error) {
+	cfg := config.New()
+
+	if err := cfg.Init(); err != nil {
+		return nil, errlib.Wrap("could not initialize configuration", err)
+	}
+
+	service := service.New(cfg)
+
+	endpoint := endpoint.New(cfg, service)
+
+	mwCors := middleware.CORS()
+
+	server := server.New(cfg, endpoint, mwCors)
+
+	return &App{
+		config:   cfg,
+		service:  service,
+		endpoint: endpoint,
+		server:   server,
+	}, nil
+}
+
+func (a *App) Run() error {
+	log.Info("service started")
+
+	if err := a.server.Start(); err != nil {
+		return errlib.Wrap("could not start http server", err)
+	}
+
+	return nil
+}
