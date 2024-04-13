7a26379f24bf1b081331711bc35f07988686dc01
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-04-13 13:31:17 +0300

    refactor(pkg/machine): rename and tidy some files

--- pkg/machine/enigma/base/base.go
+++ pkg/machine/enigma/base/base.go
@@ -1,5 +1,19 @@
 package base
 
+const (
+	CharsCount26 byte = 26
+
+	ReflectorsCount1 = 1
+	RotorsCount3     = 3
+	RotorsCount4     = 4
+	PlugboardsCount1 = 1
+
+	UpperA byte = 65
+	UpperZ byte = 90
+	LowerA byte = 97
+	LowerZ byte = 122
+)
+
 type Reflectorer interface {
 	Forward(c byte) byte
 }
